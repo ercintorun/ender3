465c37d204f2ea182292fdfaa65f1aecb3d40345
ercintorun <ercintorun@gmail.com>
2024-05-11 23:41:39 +0300

    Update printer.sh

--- klipper_android_kiauh_files/printer.sh
+++ klipper_android_kiauh_files/printer.sh
@@ -1,8 +1,22 @@
 !/bin/bash
 rm -rf /home/pi/printer_data/logs/*
 echo "logs cleaned"
+rm -rf /var/log/
+echo "/var/log cleaned"
+rm -rf /tmp/*
+echo "/tmp cleaned"
+rm -rf /var/cache/*
+echo "/var/cache cleaned"
+rm -rf /root/.cache/*
+echo "/root/.cache cleaned"
+rm -rf ./.cache/*
+echo "./.cache cleaned"
+chown pi /dev/ttyACM0
+echo "ttyttyACM0 chown pi"
 chmod 777 /dev/ttyACM0
 echo "ttyttyACM0 chmod configured"
+sudo chmod a=rwX /tmp/printer
+echo "klipper tmp-printer chmod changed"
 service klipper restart
 echo "klipper service restarted"
 sleep 2
